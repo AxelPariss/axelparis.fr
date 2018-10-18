38abec0b44c580856e7ffb8dac3267903dcce466
Axel Paris <contact@axelparis.fr>
2018-10-18 22:20:36 +0200

    Add Firebase connection

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 import './Blog.css'
+import firebase from './../Config/Firebase'
 
 class Blog extends Component {
+  constructor(){
+    super()
+    this.state = {
+      articles: []
+    }
+  }
+  componentWillMount(){
+    const ref = firebase.database().ref('articles')
+
+    ref.on('value', (snapshot) => {
+      this.setState({articles: snapshot.val()})
+    })
+  }
   render() {
+      let articlesList = this.state.articles.map((article, key) => (
+        <div className="article" key={key}>
+          <h3>{article.title}</h3>
+          <p>{article.content}</p>
+        </div>
+      ))
+
     return (
       <div className="blog">
         <h1>Blog</h1>
+        <div className="articles">
+          {(this.state.articles.length > 0) ? articlesList : 'chargement...'}        
+        </div>
       </div>
     )
   }
