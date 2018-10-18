a086f3e31681ae02aadbc58e9c5ffe25ed377d43
Axel Paris <contact@axelparis.fr>
2018-10-18 14:20:41 +0200

    Udpate HomePage

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import './Menu.css'
+import logoImage from './img/logo.png'
+
 
 class Menu extends Component {
   render() {
     return (
-      <div className="menu">
-        <a href="#">Blog</a>
-        <a href="#">Projets</a>
-        <a href="#">Portfolio</a>
-        <a href="#">Contact</a>
-      </div>
+      <header>
+        <div className="logo">
+          <img src={logoImage} alt="Logo" />
+        </div>
+        <div className="menu">
+          <a href="#">Blog</a>
+          <a href="#">Projets</a>
+          <a href="#">Portfolio</a>
+          <a href="#">Contact</a>
+        </div>
+      </header>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import './Layout.css'
+
 import Home from './Components/Home/Home'
 
 ReactDOM.render(<Home />, document.getElementById('root'));
