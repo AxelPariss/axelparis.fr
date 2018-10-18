04af598146f8faf8c72b6ad2a068e8ebae372a65
Axel Paris <contact@axelparis.fr>
2018-10-18 14:41:29 +0200

    Update HomePage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './Home.css'
 import Menu from './../Menu/Menu'
 import profileImage from './img/axel-paris.png'
+import leftArrowImage from './img/left-arrow.svg'
 
 class Home extends Component {
   render() {
@@ -16,7 +17,7 @@ class Home extends Component {
             <h1>Axel Paris</h1>
             <h2><span className="horizontal-bar"></span>#<span className="word">developer</span></h2> 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque ipsum eu augue vestibulum lacinia. Duis tincidunt et eros eget facilisis. Nullam id ultrices nibh. Duis tincidunt et eros eget facilisis. Nullam id ultrices nibh.</p>
-            <button className="hire-button">Me recruter</button>
+            <button className="hire-button">Me recruter<img src={leftArrowImage} alt="Fleche droite"/></button>
           </div>
         </div>
       </div>
